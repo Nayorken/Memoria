6248bfa8548a46fb834d84c007004384737b8793
hugo ferreira <hugoytbrg.2002@gmail.com>
2019-10-01 17:58:59 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,291 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.P12 = New System.Windows.Forms.PictureBox()
+        Me.P11 = New System.Windows.Forms.PictureBox()
+        Me.P10 = New System.Windows.Forms.PictureBox()
+        Me.P9 = New System.Windows.Forms.PictureBox()
+        Me.P8 = New System.Windows.Forms.PictureBox()
+        Me.P7 = New System.Windows.Forms.PictureBox()
+        Me.P6 = New System.Windows.Forms.PictureBox()
+        Me.P5 = New System.Windows.Forms.PictureBox()
+        Me.P4 = New System.Windows.Forms.PictureBox()
+        Me.P3 = New System.Windows.Forms.PictureBox()
+        Me.P2 = New System.Windows.Forms.PictureBox()
+        Me.P1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.P12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'P12
+        '
+        Me.P12.BackgroundImage = CType(resources.GetObject("P12.BackgroundImage"), System.Drawing.Image)
+        Me.P12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P12.Location = New System.Drawing.Point(486, 355)
+        Me.P12.Name = "P12"
+        Me.P12.Size = New System.Drawing.Size(128, 128)
+        Me.P12.TabIndex = 11
+        Me.P12.TabStop = False
+        '
+        'P11
+        '
+        Me.P11.BackgroundImage = CType(resources.GetObject("P11.BackgroundImage"), System.Drawing.Image)
+        Me.P11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P11.Location = New System.Drawing.Point(327, 355)
+        Me.P11.Name = "P11"
+        Me.P11.Size = New System.Drawing.Size(128, 128)
+        Me.P11.TabIndex = 10
+        Me.P11.TabStop = False
+        '
+        'P10
+        '
+        Me.P10.BackgroundImage = CType(resources.GetObject("P10.BackgroundImage"), System.Drawing.Image)
+        Me.P10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P10.Location = New System.Drawing.Point(173, 355)
+        Me.P10.Name = "P10"
+        Me.P10.Size = New System.Drawing.Size(128, 128)
+        Me.P10.TabIndex = 9
+        Me.P10.TabStop = False
+        '
+        'P9
+        '
+        Me.P9.BackgroundImage = CType(resources.GetObject("P9.BackgroundImage"), System.Drawing.Image)
+        Me.P9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P9.Location = New System.Drawing.Point(14, 355)
+        Me.P9.Name = "P9"
+        Me.P9.Size = New System.Drawing.Size(128, 128)
+        Me.P9.TabIndex = 8
+        Me.P9.TabStop = False
+        '
+        'P8
+        '
+        Me.P8.BackgroundImage = CType(resources.GetObject("P8.BackgroundImage"), System.Drawing.Image)
+        Me.P8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P8.Location = New System.Drawing.Point(486, 200)
+        Me.P8.Name = "P8"
+        Me.P8.Size = New System.Drawing.Size(128, 128)
+        Me.P8.TabIndex = 7
+        Me.P8.TabStop = False
+        '
+        'P7
+        '
+        Me.P7.BackgroundImage = CType(resources.GetObject("P7.BackgroundImage"), System.Drawing.Image)
+        Me.P7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P7.Location = New System.Drawing.Point(327, 200)
+        Me.P7.Name = "P7"
+        Me.P7.Size = New System.Drawing.Size(128, 128)
+        Me.P7.TabIndex = 6
+        Me.P7.TabStop = False
+        '
+        'P6
+        '
+        Me.P6.BackgroundImage = CType(resources.GetObject("P6.BackgroundImage"), System.Drawing.Image)
+        Me.P6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P6.Location = New System.Drawing.Point(173, 200)
+        Me.P6.Name = "P6"
+        Me.P6.Size = New System.Drawing.Size(128, 128)
+        Me.P6.TabIndex = 5
+        Me.P6.TabStop = False
+        '
+        'P5
+        '
+        Me.P5.BackgroundImage = CType(resources.GetObject("P5.BackgroundImage"), System.Drawing.Image)
+        Me.P5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P5.Location = New System.Drawing.Point(14, 200)
+        Me.P5.Name = "P5"
+        Me.P5.Size = New System.Drawing.Size(128, 128)
+        Me.P5.TabIndex = 4
+        Me.P5.TabStop = False
+        '
+        'P4
+        '
+        Me.P4.BackgroundImage = CType(resources.GetObject("P4.BackgroundImage"), System.Drawing.Image)
+        Me.P4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P4.Location = New System.Drawing.Point(486, 49)
+        Me.P4.Name = "P4"
+        Me.P4.Size = New System.Drawing.Size(128, 128)
+        Me.P4.TabIndex = 3
+        Me.P4.TabStop = False
+        '
+        'P3
+        '
+        Me.P3.BackgroundImage = CType(resources.GetObject("P3.BackgroundImage"), System.Drawing.Image)
+        Me.P3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P3.Location = New System.Drawing.Point(327, 49)
+        Me.P3.Name = "P3"
+        Me.P3.Size = New System.Drawing.Size(128, 128)
+        Me.P3.TabIndex = 2
+        Me.P3.TabStop = False
+        '
+        'P2
+        '
+        Me.P2.BackgroundImage = CType(resources.GetObject("P2.BackgroundImage"), System.Drawing.Image)
+        Me.P2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P2.Location = New System.Drawing.Point(173, 49)
+        Me.P2.Name = "P2"
+        Me.P2.Size = New System.Drawing.Size(128, 128)
+        Me.P2.TabIndex = 1
+        Me.P2.TabStop = False
+        '
+        'P1
+        '
+        Me.P1.BackgroundImage = CType(resources.GetObject("P1.BackgroundImage"), System.Drawing.Image)
+        Me.P1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.P1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.P1.Location = New System.Drawing.Point(14, 49)
+        Me.P1.Name = "P1"
+        Me.P1.Size = New System.Drawing.Size(128, 128)
+        Me.P1.TabIndex = 0
+        Me.P1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Red
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(-12, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(660, 41)
+        Me.Panel1.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.75!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.Location = New System.Drawing.Point(603, 4)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 32)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "X"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Modern No. 20", 21.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label2.Location = New System.Drawing.Point(216, 6)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(236, 30)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Jogo Da Memória "
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Red
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Button1.Location = New System.Drawing.Point(236, 494)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(150, 29)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Novo Jogo"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Linen
+        Me.ClientSize = New System.Drawing.Size(629, 539)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.P12)
+        Me.Controls.Add(Me.P11)
+        Me.Controls.Add(Me.P10)
+        Me.Controls.Add(Me.P9)
+        Me.Controls.Add(Me.P8)
+        Me.Controls.Add(Me.P7)
+        Me.Controls.Add(Me.P6)
+        Me.Controls.Add(Me.P5)
+        Me.Controls.Add(Me.P4)
+        Me.Controls.Add(Me.P3)
+        Me.Controls.Add(Me.P2)
+        Me.Controls.Add(Me.P1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.P12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents P1 As PictureBox
+    Friend WithEvents P2 As PictureBox
+    Friend WithEvents P4 As PictureBox
+    Friend WithEvents P3 As PictureBox
+    Friend WithEvents P8 As PictureBox
+    Friend WithEvents P7 As PictureBox
+    Friend WithEvents P6 As PictureBox
+    Friend WithEvents P5 As PictureBox
+    Friend WithEvents P12 As PictureBox
+    Friend WithEvents P11 As PictureBox
+    Friend WithEvents P10 As PictureBox
+    Friend WithEvents P9 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+End Class
