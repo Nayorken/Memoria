f2d206c0a61d870468cffb56f9d69b676f7592b4
hugo ferreira <hugoytbrg.2002@gmail.com>
2019-10-03 10:53:54 +0100

    up 07

--- Form1.vb
+++ Form1.vb
@@ -1,10 +1,16 @@
 ﻿Public Class Form1
     Dim Matriz(12) As Integer
+    Dim Jogadas(12) As Integer
+    Dim Ajogar
+    Dim Anterior As Integer
     Dim Quadros() As PictureBox
     Private Sub Inicializa()
         Dim i As Integer
+        Ajogar = 1
+        Randomize()
         For i = 0 To 11
             Matriz(i) = 0
+            Jogadas(i) = 0
             Quadros(i).BackgroundImage = My.Resources.vazio
         Next
 
@@ -26,24 +32,49 @@
             Case 4 : fig.BackgroundImage = My.Resources._5
             Case 5 : fig.BackgroundImage = My.Resources._6
         End Select
-
         Quadros(quadro).BackgroundImage = fig.BackgroundImage
+        Jogadas(quadro) = 1
+        Refresh()
+        Threading.Thread.Sleep(500)
+        If Ajogar = 1 Then : Anterior = quadro
+        ElseIf Matriz(Anterior) <> Matriz(quadro) Then
+            Quadros(quadro).BackgroundImage = My.Resources.vazio
+            Jogadas(quadro) = 0
+            Quadros(Anterior).BackgroundImage = My.Resources.vazio
+            Jogadas(Anterior) = 0
+        End If
     End Sub
     Private Sub Clicar(sender As Object, e As EventArgs) Handles P1.Click, P2.Click, P4.Click, P3.Click, P9.Click, P8.Click, P7.Click, P6.Click, P5.Click, P12.Click, P11.Click, P10.Click
+        Dim quadro As Integer
         Select Case sender.name
-            Case "P1" : Imagem(0)
-            Case "P2" : Imagem(1)
-            Case "P3" : Imagem(2)
-            Case "P4" : Imagem(3)
-            Case "P5" : Imagem(4)
-            Case "P6" : Imagem(5)
-            Case "P7" : Imagem(6)
-            Case "P8" : Imagem(7)
-            Case "P9" : Imagem(8)
-            Case "P10" : Imagem(9)
-            Case "P11" : Imagem(10)
-            Case "P12" : Imagem(11)
+            Case "P1" : quadro = 0
+            Case "P2" : quadro = 1
+            Case "P3" : quadro = 2
+            Case "P4" : quadro = 3
+            Case "P5" : quadro = 4
+            Case "P6" : quadro = 5
+            Case "P7" : quadro = 6
+            Case "P8" : quadro = 7
+            Case "P9" : quadro = 8
+            Case "P10" : quadro = 9
+            Case "P11" : quadro = 10
+            Case "P12" : quadro = 11
         End Select
+        If Jogadas(quadro) Then Return
+        Imagem(quadro)
+        If Ajogar = 1 Then : Ajogar = 2
+        Else : Ajogar = 1
+        End If
+        Dim ganhou = True
+        For i = 0 To 11
+            If Jogadas(i) = 0 Then ganhou = False
+        Next
+        If ganhou Then
+            Beep()
+            MsgBox("Ganhouuuuu ",, "Fim do Jogo")
+            Call Inicializa()
+        End If
+
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
